0e70113fdd8e6c699192cad95f6bbb1d9c96e328
RyujiShimatani <138374692+RyujiShimatani@users.noreply.github.com>
2023-07-12 07:39:57 +0000

    新規投稿機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,13 @@ class BooksController < ApplicationController
   end
 
   def index
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
   end
 
   def show
@@ -13,4 +20,10 @@ class BooksController < ApplicationController
 
   def update
   end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
