aa6f81c5dda86e9c65a361ab0986d8d5c64ae6f7
RyujiShimatani <138374692+RyujiShimatani@users.noreply.github.com>
2023-07-13 12:06:48 +0000

    レイアウト変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
       redirect_to '/books'
-      flash[:notice] = "投稿が成功しました"
+      flash[:notice] = "投稿が成功しました,successfully"
     else
       @books = Book.all
       render :index
@@ -30,7 +30,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book.id)
-      flash[:notice] = "編集が成功しました"
+      flash[:notice] = "編集が成功しました,successfully"
     else
       render :edit
     end
